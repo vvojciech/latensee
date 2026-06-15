2d309b6bb3c385e4c010fc25fcb6457a8ddd709c
Wojciech Kucha <wojciech.kucha@simplybusiness.co.uk>
2026-06-15 14:39:16 +0100

    [Feat] Define HopState and TraceState data model

--- src/trace/state.rs
+++ src/trace/state.rs
@@ -1,1 +1,12 @@
-// Core data model - to be implemented
+use std::net::IpAddr;
+
+#[derive(Debug, Clone)]
+pub struct HopState {
+    pub addr: Option<IpAddr>,
+    pub hostname: Option<String>,
+}
+
+#[derive(Debug, Default)]
+pub struct TraceState {
+    pub hops: Vec<HopState>,
+}
