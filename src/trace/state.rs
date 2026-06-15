a3aaae0a7e2a3d7c2b4ff2dd7d0e8774a43c2f2d
Wojciech Kucha <wojciech.kucha@simplybusiness.co.uk>
2026-06-15 14:37:54 +0100

    [Feat] Implement TraceState data model types
    TargetInfo, HopState, HopStats structs for trace result representation.

--- src/trace/state.rs
+++ src/trace/state.rs
@@ -1,1 +1,38 @@
-// Core data model - to be implemented
+use std::net::IpAddr;
+use std::time::{Duration, Instant};
+
+#[derive(Debug, Clone)]
+pub struct TargetInfo {
+    pub hostname: String,
+    pub addr: IpAddr,
+}
+
+#[derive(Debug, Clone)]
+pub struct HopStats {
+    pub sent: u64,
+    pub received: u64,
+    pub lost: u64,
+    pub loss_pct: f64,
+    pub last_rtt: Option<Duration>,
+    pub min_rtt: Option<Duration>,
+    pub max_rtt: Option<Duration>,
+    pub avg_rtt: f64,   // microseconds
+    pub jitter: f64,    // microseconds
+}
+
+#[derive(Debug, Clone)]
+pub struct HopState {
+    pub ttl: u8,
+    pub addr: Option<IpAddr>,
+    pub hostname: Option<String>,
+    pub samples: Vec<Duration>,
+    pub stats: HopStats,
+}
+
+#[derive(Debug)]
+pub struct TraceState {
+    pub target: TargetInfo,
+    pub hops: Vec<HopState>,
+    pub round: u64,
+    pub started_at: Instant,
+}
